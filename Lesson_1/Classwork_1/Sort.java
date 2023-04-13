eb1cfb8e2046cb65a45ad3ead22979826fb4a456
SlavaVasilakiy <slk55slk@gmail.com>
2023-04-13 22:38:47 +0300

    Lesson 1

--- Lesson_1/Classwork_1/Sort.java
+++ Lesson_1/Classwork_1/Sort.java
@@ -2,12 +2,12 @@
 public class Sort {
 
 	public static void main(String[] args) {
-		int[] array = new int[]{4, 2, 5, 1, 9, 3, 7, 6, 8, 0};
+		int[] array = new int[]{4, 2, 5, 1, 9, 3, 7, 6, 8, 0, 1};
 
 		// bubbleSort(array);
 		// directSort(array);
 		// insertSort(array);
-		// quickSort(array,0, array.length - 1);
+		// quickSort(array);
 		heapifySortCreate(array);
 
 		for (int j : array) {
@@ -17,7 +17,6 @@ public class Sort {
 
 	/**
 	 * Сортировка пузырьком.
-	 *
 	 * Один из наиболее простых алгоритмов сортировки массива – пузырьковая
 	 * сортировка. Нагляднее всего работу этого алгоритма можно продемонстрировать в
 	 * вертикальном массиве – наиболее легкие элементы стремятся вверх, словно
@@ -203,6 +202,10 @@ public class Sort {
 		}
 	}
 
+	public static void quickSort(int[] array) {
+		quickSort(array, 0, array.length - 1);
+	}
+
 	/**
 	 * Сортировка кучей (пирамидальная).
 	 *Особенность данной сортировки в использовании дополнительной структуры
@@ -289,4 +292,78 @@ public class Sort {
 		// Рекурсивно преобразуем в двоичную кучу затронутое поддерево.
 		heapifySort(array, heapSize, largest);
 	}
+
+/*	// Реализация пирамидальной сортировки на Java
+	public class HeapSort
+	{
+		public void sort(int arr[])
+		{
+			int n = arr.length;
+
+			// Построение кучи (перегруппируем массив)
+			for (int i = n / 2 - 1; i >= 0; i--)
+				heapify(arr, n, i);
+
+			// Один за другим извлекаем элементы из кучи
+			for (int i=n-1; i>=0; i--)
+			{
+				// Перемещаем текущий корень в конец
+				int temp = arr[0];
+				arr[0] = arr[i];
+				arr[i] = temp;
+
+				// Вызываем процедуру heapify на уменьшенной куче
+				heapify(arr, i, 0);
+			}
+		}
+
+		// Процедура для преобразования в двоичную кучу поддерева с корневым узлом i, что является
+// индексом в arr[]. n - размер кучи
+		void heapify(int arr[], int n, int i)
+		{
+			int largest = i; // Инициализируем наибольший элемент как корень
+			int l = 2*i + 1; // левый = 2*i + 1
+			int r = 2*i + 2; // правый = 2*i + 2
+
+			// Если левый дочерний элемент больше корня
+			if (l < n && arr[l] > arr[largest])
+				largest = l;
+
+			// Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+			if (r < n && arr[r] > arr[largest])
+				largest = r;
+			// Если самый большой элемент не корень
+			if (largest != i)
+			{
+				int swap = arr[i];
+				arr[i] = arr[largest];
+				arr[largest] = swap;
+
+				// Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+				heapify(arr, n, largest);
+			}
+		}
+
+		*//* Вспомогательная функция для вывода на экран массива размера n *//*
+		static void printArray(int arr[])
+		{
+			int n = arr.length;
+			for (int i=0; i<n; ++i)
+				System.out.print(arr[i]+" ");
+			System.out.println();
+		}
+
+		// Управляющая программа
+		public static void main(String args[])
+		{
+			int arr[] = {12, 11, 13, 5, 6, 7};
+			int n = arr.length;
+
+			HeapSort ob = new HeapSort();
+			ob.sort(arr);
+
+			System.out.println("Sorted array is");
+			printArray(arr);
+		}
+	}*/
 }
